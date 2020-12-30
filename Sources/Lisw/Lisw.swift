02b3706d3854a1b9155e83e5f952888df8e087e4
oiltypeblur <72696185+oiltypeblur@users.noreply.github.com>
2020-12-30 18:39:15 +0900

    test: add case for quote

--- Sources/Lisw/Lisw.swift
+++ Sources/Lisw/Lisw.swift
@@ -1,19 +1,46 @@
+import Foundation
 struct Lisw {
     var text = "Hello, World!"
 }
 
 enum SExpr : Equatable {
+    case Symbol(String)
     case Number(Double)
+    case List([SExpr])
 }
 
 func tokenize(input:String)->[String]{
-    return [input]
+    var tmp = input.replacingOccurrences(of: "(", with: " ( ")
+    tmp = tmp.replacingOccurrences(of: ")", with: " ) ")
+    tmp = tmp.trimmingCharacters(in: .whitespaces)
+    return tmp.components(separatedBy: " ")
 }
 
-func readFrom(tokens:[String])->SExpr{
-    return .Number(Double(tokens[0])!)
+func readFrom(tokens:[String], startIndex: Int)->SExpr{
+    precondition(!tokens.isEmpty)
+    
+    switch tokens[startIndex] {
+    case "(":
+        var stack = [SExpr]()
+        var index = startIndex + 1
+        while tokens[index] != ")" {
+            stack.append(readFrom(tokens: tokens, startIndex: index))
+            index += 1
+        }
+        return .List(stack)
+    default:
+        return atom(token:tokens[startIndex])
+    }
+}
+
+func atom(token:String)->SExpr{
+    if let num = Double(token) {
+        return .Number(num)
+    } else {
+        return .Symbol(token)
+    }
 }
 
 func parse(input:String)->SExpr{
-    return readFrom(tokens: tokenize(input: input))
+    return readFrom(tokens: tokenize(input: input), startIndex:0)
 }
--- Tests/LiswTests/LiswTests.swift
+++ Tests/LiswTests/LiswTests.swift
@@ -3,26 +3,31 @@ import XCTest
 
 final class LiswTests: XCTestCase {
     func testTokenize(){
-        XCTContext.runActivity(named: "number"){ _ in
-            let actual = tokenize(input: "10")
-            XCTAssertEqual(actual, ["10"])
-        }
+        var actual = tokenize(input: "10")
+        XCTAssertEqual(actual, ["10"])
+
+        actual = tokenize(input: "(1 2)")
+        XCTAssertEqual(actual, ["(", "1", "2", ")"])
     }
     
     func testReadFrom(){
-        XCTContext.runActivity(named: "number"){_ in
-            let actual = readFrom(tokens: ["20"])
+        XCTContext.runActivity(named: "number"){ _ in
+            let actual = readFrom(tokens: ["20"], startIndex:0)
             XCTAssertEqual(actual, .Number(20))
         }
     }
     
     func testParse(){
         // var
-        XCTContext.runActivity(named: "number"){_ in
+        XCTContext.runActivity(named: "number"){ _ in
             let actual = parse(input:"30")
             XCTAssertEqual(actual, .Number(30))
         }
         // quote
+        XCTContext.runActivity(named: "quote"){ _ in
+            let actual = parse(input:"(quote 40)")
+            XCTAssertEqual(actual, .List([.Symbol("quote"), .Number(40)]))
+        }
         // if
         // set!
         // define
